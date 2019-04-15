2ea6a44db7df9384b8bc73d03b21f88db5fad4f6
Gabriela Knafel <gabriela.knafel@gmail.com>
2019-04-15 16:03:36 +0200

    Primary configuration

---
+++ src/main/java/com/github/knafelek/pilesapi/PilesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.knafelek.pilesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilesApiApplication.class, args);
+	}
+
+}
